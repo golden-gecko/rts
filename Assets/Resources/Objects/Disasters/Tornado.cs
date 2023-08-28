8ff167eb7cadf9b571ec562c19a9853915949aad
Wojciech Holisz <golden.gecko@holisz.pl>
2023-08-28 13:53:27 +0200

    Move hardcoded values into config class

--- Assets/Resources/Objects/Disasters/Tornado.cs
+++ Assets/Resources/Objects/Disasters/Tornado.cs
@@ -6,8 +6,8 @@ public class Tornado : Disaster
     {
         base.Awake();
 
-        float x = Position.x + Random.Range(-200.0f, 200.0f); // TODO: Hardcoded.
-        float z = Position.z + Random.Range(-200.0f, 200.0f);
+        float x = Position.x + Random.Range(-Config.DisasterDirection, Config.DisasterDirection);
+        float z = Position.z + Random.Range(-Config.DisasterDirection, Config.DisasterDirection);
 
         Move(new Vector3(x, 0.0f, z));
     }
