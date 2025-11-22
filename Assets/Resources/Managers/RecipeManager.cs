1bde736165f2fce50e80d27346c926adae7a99ae
golden-gecko <golden-gecko@holisz.pl>
2025-11-22 13:04:55 +0100

    Remove object when construct order is cancelled

--- Assets/Resources/Managers/RecipeManager.cs
+++ Assets/Resources/Managers/RecipeManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class RecipeManager : MonoBehaviour
@@ -15,27 +16,38 @@ public class RecipeManager : MonoBehaviour
             Instance = this;
         }
 
-        Recipe r1 = new Recipe("Charcoal");
-        Recipe r2 = new Recipe("Iron");
+        CreateRecipes();
+    }
 
+    public Recipe Get(string name)
+    {
+        return Recipes.Get(name);
+    }
+
+    private void CreateRecipes()
+    {
+        Recipe r1 = new Recipe("Charcoal");
         // <=
         r1.Consumes("Wood", 2);
         // =>
         r1.Produces("Coal", 1);
 
+        Recipe r2 = new Recipe("Iron");
         // <=
         r2.Consumes("Coal", 2);
         r2.Consumes("Iron Ore", 1);
         // =>
         r2.Produces("Iron", 1);
 
+        Recipe r3 = new Recipe("Fuel");
+        // <=
+        r1.Consumes("Crude Oil", 1);
+        // =>
+        r1.Produces("Fuel", 1);
+
         Recipes.Add(r1);
         Recipes.Add(r2);
-    }
-
-    public Recipe Get(string name)
-    {
-        return Recipes.Get(name);
+        Recipes.Add(r3);
     }
 
     private RecipeContainer Recipes = new RecipeContainer();
