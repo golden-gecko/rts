30098a7f3b672e3d79c1f50948129f5d3b7decbd
golden-gecko <golden-gecko@holisz.pl>
2025-11-22 13:04:22 +0100

    Add turrets (#11)

---
+++ Assets/Resources/Objects/Structures/struct_Factory_Light_A_yup.cs
@@ -0,0 +1,24 @@
+public class struct_Factory_Light_A_yup : Structure
+{
+    protected override void Awake()
+    {
+        base.Awake();
+
+        Orders.AllowOrder(OrderType.Construct);
+        Orders.AllowOrder(OrderType.Rally);
+
+        Orders.AllowPrefab("Objects/Vehicles/unit_Infantry_Light_B_yup");
+        Orders.AllowPrefab("Objects/Vehicles/unit_Grav_Light_A_yup");
+        Orders.AllowPrefab("Objects/Vehicles/unit_Harvester_A_yup");
+        Orders.AllowPrefab("Objects/Vehicles/unit_Quad_A_yup");
+        Orders.AllowPrefab("Objects/Vehicles/unit_Trike_A_yup");
+        
+        Resources.Add("Metal", 0, 40);
+
+        Recipe r1 = new Recipe();
+
+        r1.Consume("Metal", 0);
+
+        Recipes.Add(r1);
+    }
+}
---
+++ Assets/Resources/Objects/Units/unit_Tank_Combat_A_yup.cs
@@ -0,0 +1,13 @@
+public class unit_Tank_Combat_A_yup : Unit
+{
+    protected override void Awake()
+    {
+        base.Awake();
+
+        Orders.AllowOrder(OrderType.Attack);
+
+        Speed = 6.0f;
+        MissilePrefab = "Objects/Missiles/Rocket";
+        ReloadTimer = new Timer(5.0f);
+    }
+}
