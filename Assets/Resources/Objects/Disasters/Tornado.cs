814338f29daf82c635083d80e26081cb8fee690b
golden-gecko <golden-gecko@holisz.pl>
2025-11-22 13:04:45 +0100

    Add move order to tornado

--- Assets/Resources/Objects/Disasters/Tornado.cs
+++ Assets/Resources/Objects/Disasters/Tornado.cs
@@ -1,3 +1,19 @@
+using UnityEngine;
+
 public class Tornado : Disaster
 {
+    protected override void Awake()
+    {
+        base.Awake();
+
+        float x = Position.x + Random.Range(-200.0f, 200.0f); // TODO: Hardcoded.
+        float z = Position.z + Random.Range(-200.0f, 200.0f);
+
+        Move(new Vector3(x, 0.0f, z));
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
 }
